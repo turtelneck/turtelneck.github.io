9d96ac969396eb5343cf054e3caeb97a31f96330
turtelneck <65428036+turtelneck@users.noreply.github.com>
2022-03-12 22:46:12 -0500

    added skeleton

--- stacksto/src/app/app.module.ts
+++ stacksto/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BoxesComponent } from './boxes/boxes.component';
+import { BoxDetailComponent } from './box-detail/box-detail.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoxesComponent,
+    BoxDetailComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ stacksto/src/app/box-detail/box-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-box-detail',
+  templateUrl: './box-detail.component.html',
+  styleUrls: ['./box-detail.component.css']
+})
+export class BoxDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
